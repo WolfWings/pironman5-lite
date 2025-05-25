37acea3ccb1a5adc0f4c375b2abb626443f1b11c
Wolf <wolfwings@gmail.com>
2025-05-24 19:32:36 -0500

    Cleaning up python generation scripts
    Standardizing in ' instead of " mostly, and cleaning up the output
    to be more human-readable and avoiding escaping issues via unicode
    character names instead of raw characters being printed.

--- generate_fonts.py
+++ generate_fonts.py
@@ -1,28 +1,33 @@
 #!/bin/env python3
 from bdfparser.src.bdfparser.bdfparser import *
+from unicodedata import name as unicodename
 
-fonts = open( "fonts.h", "w" )
+fonts = open( 'fonts.h', 'w' )
 
 def process_font( filename ):
 	f = Font( filename )
 
 	width = f.headers[ 'fbbx' ]
 	height = f.headers[ 'fbby' ]
-	print( f"Processing { height } pixel tall font..." )
+	print( f'Processing { height } pixel tall font...' )
 	if ( height % 8 ) != 0:
-		raise Exception( f"Font generator only supports fonts a multiple of 8 pixels tall" )
+		raise Exception( f'Font generator only supports fonts a multiple of 8 pixels tall' )
 
-	print( f"const unsigned char font_{ height // 8 }[] = {{", file = fonts )
+	print( f'const unsigned char font_{ height // 8 }[] = {{', file = fonts )
 
 	for c in range( 32, 126 + 1 ):
+		cname = unicodename( chr( c ) )
+
 		g = f.glyph( chr( c ) )
 		if g is None:
-			g = f.glyph( " " )
+			g = f.glyph( ' ' )
 			if g is None:
-				raise Exception( f"Missing glyph [{ chr( c ) }] and no space character" )
+				raise Exception( f'Missing glyph [{ cname }] and no space character' )
 
 		bmp = g.draw().todata( 2 )
 
+		# Only added to first line
+		trailer = f' // { cname }'
 		for y in range( 0, height - 1, 8 ):
 			for x in range( width ):
 				bits = 0
@@ -31,15 +36,13 @@ def process_font( filename ):
 						bits |= ( 1 << yo )
 				print( '0x%02x,' % bits, end = '', file = fonts )
 
-			if y == 0:
-				print( f" // Character \"{ chr( c ) }\"", end = '', file = fonts )
+			print( trailer, file = fonts )
+			trailer = ''
 
-			print( '', file = fonts )
+	print( '};', file = fonts )
 
-	print( "};", file = fonts )
+print( 'static const int font_widths[ 4 ] = { 5, 8, 12, 16 };', file = fonts )
 
-print( "static const int font_widths[ 4 ] = { 5, 8, 12, 16 };", file = fonts )
+[ process_font( f'spleen_font/spleen-{ size }.bdf' ) for size in [ '5x8', '8x16', '12x24', '16x32' ] ]
 
-[ process_font( f"spleen_font/spleen-{ size }.bdf" ) for size in [ "5x8", "8x16", "12x24", "16x32" ] ]
-
-print( "static const unsigned char *fonts[ 4 ] = { font_1, font_2, font_3, font_4 };", file = fonts )
+print( 'static const unsigned char *fonts[ 4 ] = { font_1, font_2, font_3, font_4 };', file = fonts )
--- generate_masks.py
+++ generate_masks.py
@@ -3,7 +3,7 @@
 import sys
 
 if len( sys.argv ) < 2:
-	print( f"Usage: { sys.argv[0] } <mask_file.txt>" )
+	print( f'Usage: { sys.argv[0] } <mask_file.txt>' )
 	exit( 0 )
 
 lines = []
@@ -13,13 +13,16 @@ with open( sys.argv[ 1 ], 'r' ) as file:
 		if len( lines ) >= 64:
 			break
 
-lines = ( lines + [ "" for _ in range( 64 ) ] )[:64]
+lines = ( lines + [ '' for _ in range( 64 ) ] )[:64]
 
-masks = open( "masks.h", "w" )
+masks = open( 'masks.h', 'w' )
 
-print( "unsigned char oled_mask_and[ 1024 ] = {", file = masks )
+print( 'unsigned char oled_mask_and[ 1024 ] = {', file = masks )
 
 for ys in range( 0, 64, 8 ):
+	if ys > 0:
+		print( '', file = masks )
+
 	for xs in range( 0, 128, 16 ):
 		for xo in range( 16 ):
 			mask = 0;
@@ -28,15 +31,15 @@ for ys in range( 0, 64, 8 ):
 				if lines[ ys + yo ][ x : x + 1 ] == 'X':
 					mask |= 1 << yo
 			mask ^= 0xFF
-			print( f"0x{mask:0>2x},", end = '', file = masks )
+			print( f'0x{mask:0>2x},', end = '', file = masks )
 		print( '', file = masks )
-	print( '', file = masks )
 
-print( "};", file = masks )
-
-print( "unsigned char oled_mask_or[ 1024 ] = {", file = masks )
+print( '};\n\nunsigned char oled_mask_or[ 1024 ] = {', file = masks )
 
 for ys in range( 0, 64, 8 ):
+	if ys > 0:
+		print( '', file = masks )
+
 	for xs in range( 0, 128, 16 ):
 		for xo in range( 16 ):
 			mask = 0;
@@ -44,8 +47,7 @@ for ys in range( 0, 64, 8 ):
 				x = xs + xo
 				if lines[ ys + yo ][ x : x + 1 ] == '+':
 					mask |= 1 << yo
-			print( f"0x{mask:0>2x},", end = '', file = masks )
+			print( f'0x{mask:0>2x},', end = '', file = masks )
 		print( '', file = masks )
-	print( '', file = masks )
 
-print( "};", file = masks )
+print( '};', file = masks )
